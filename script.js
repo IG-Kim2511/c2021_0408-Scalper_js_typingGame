77be8c0aa2783a1c84e92a4a82361f522abe55a7
iikim <iikim2511@gmail.com>
2021-04-11 11:42:52 -0400

    no message

--- script.js
+++ script.js
@@ -10,28 +10,19 @@ let checkInterval;
 const wordInput = document.querySelector('.word-input');
 const wordDisplay = document.querySelector('.word-display');
 const scoreDisplay = document.querySelector('.score');
+
 const timeDisplay = document.querySelector('.time');        // js 10 
 const button = document.querySelector('.button');
-
+const buttonStop = document.querySelector('.button_stop');
 const header = document.querySelector('.header');
 
-
-
-// js 12 initiate value
-
-init();
-
-function init(){
-    getWords()
-    wordInput.addEventListener('input',checkMatch())            //js 12
-}
 
 
 // js 2, when those values are same, score++
 
 // wordInput.addEventListener('input',checkMatch())             //js 12
 
-function checkMatch() {
+/* function checkMatch() {
     if(wordInput.value.toLowerCase() === wordDisplay.innerHTML.toLowerCase()){
         score++;
         scoreDisplay.innerHTML = score;
@@ -45,31 +36,12 @@ function checkStatus() {               //js 12-4
         clearInterval(checkInterval)      //js 12-4  
     }
     
-}
-
-/* wordInput.addEventListener('input',()=>{
-    
-    if(wordInput.value.toLowerCase() === wordDisplay.innerHTML.toLowerCase()){
-        score++;
-        scoreDisplay.innerHTML = score;
-        wordInput.value="";
-        
-    }
-}) */
- 
-// js 8 buttonChange
-// buttonChange('game start');          //js 12
-
-function buttonChange(a_text) {
-    button.innerHTML = a_text;
-    a_text === 'game start' ? button.classList.remove('loading')  : button.classList.add('loading');
-    
-}
-
+} */
 
 
 //  js  4, click button, and countdown
-async function countDown() {
+
+/* async function countDown() {
     time > 0 ? time-- : isPlaying = false;
 
     if (isPlaying) {
@@ -81,19 +53,85 @@ async function countDown() {
 }
 
 countDown().then( ()=>{ let time = Game_time;}
-    )
+    ) */
+
+
 // js 4-3
 
-
-function run() {
+/* function run() {
     // isPlaying=true;
     time= Game_time;
    timeInterval = setInterval(countDown, 1000);    // js 4-2
-   checkInterval = setInterval(checkStatus,50)            //js 12-4       
+   checkInterval = setInterval(checkStatus,50)            //js 12-4           
+}
+button.addEventListener('click',run(), countDown(),buttonChange('loading'));
+ */
+
+
+button.addEventListener('click', run);
+
+function run(){
+    /* 1. 버튼이 게임중으로 표시
+    2. 카운트 다운
+        2-1 . 맞추면 스코어 , 틀리면 다음단어로
+    3 . new word show
+    4. 단어 맞추면 스코어 */
+    buttonChange('gaming...');
+    button.classList.add('loading');
+
+    countDown()
+}
+
+function countDown() {
+    
+    // if (time > 0) {
+    //     time--;        
+    // }  
+    // timeDisplay.innerHTML = time;
+    timeDisplay.innertext = time;
+}
+
+
+
+// js 5 Stop game
+
+buttonStop.addEventListener('click',stop);
+
+function stop(params) {
     
 }
 
-button.addEventListener('click',run(), countDown(),buttonChange('loading'));
+
+// js 8 buttonChange
+
+function buttonChange(a_text) {
+    button.innerHTML = a_text;
+        
+}
+
+
+
+
+// buttonChange('game start');          //js 12
+
+/* function buttonChange(a_text) {
+    button.innerHTML = a_text;
+    a_text === 'game start' ? button.classList.remove('loading')  : button.classList.add('loading');
+    
+}
+ */
+
+
+// js 12 initiate value
+
+/* init();
+
+function init(){
+    getWords()
+    wordInput.addEventListener('input',checkMatch())            //js 12
+}
+
+ */
 
 
 // js 16 axios & words array
@@ -124,7 +162,7 @@ function getWords() {
   
   console.log(words)
   
-  buttonChange('game start');              //js 12
+//   buttonChange('game start');              //js 12
 
 
 }
